8dffee7e9e79a8b0d2c17f5988677e0847d4768c
DanielJesus1189 <2231189@my.ipleiria.pt>
2026-08-31 13:41:33 +0000

    Deploy fullstack app with Docker and Cloudflare Tunnel

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -6,12 +6,14 @@ import dotenv from "dotenv";
 
 dotenv.config();
 
-const PORT = process.env.PORT || 4000;
+const PORT = Number(process.env.PORT) || 4000;
 const MONGO_URI = process.env.MONGO_URI || "";
 
 const app = express();
 app.use(express.json());
-app.use(cors({}));
+app.use(cors({
+    origin: "https://app.diselab.pt",
+}));
 
 mongoose.connect(MONGO_URI, {
     dbName: "node-typescript-app",
@@ -25,6 +27,6 @@ mongoose.connect(MONGO_URI, {
 
 app.use("/", router);
 
-app.listen(PORT, () => {
-    console.log("Server is running on port " + "http://localhost:" + PORT);
+app.listen(PORT, "0.0.0.0", () => {
+    console.log("Server is running on port " + PORT);
 });
